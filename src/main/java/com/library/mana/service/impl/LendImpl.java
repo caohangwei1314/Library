f44fcb324979724838a91a3caf5d082b6134dc1d
caohangwei1314 <104079381@qq.com>
2019-05-28 08:27:17 +0800

    【修改】归还图书时扣款

--- src/main/java/com/library/mana/service/impl/LendImpl.java
+++ src/main/java/com/library/mana/service/impl/LendImpl.java
@@ -7,6 +7,7 @@ import com.library.mana.utils.PageBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 @Service
@@ -40,11 +41,16 @@ public class LendImpl implements LendService {
         if(booksMapper.count(re)<1)
             return 0;
         Users users = usersMapper.selectByPrimaryKey(record.getUserId());
+        users.setNum(users.getNum()+1);
         Authority authority = authorityMapper.selectByClassId(users.getClassId());
+        if(users.getNum()>authority.getNumber())
+            return 4;
         Conditions conditions = new Conditions();
         conditions.setUserId(users.getPkId());
         conditions.setIsReturn(0);
         if(booksBorrowMapper.count(conditions)>authority.getNumber())
+            return 0;
+        if(usersMapper.updateByPrimaryKeySelective(users)<1)
             return 0;
         Long time = System.currentTimeMillis();
         record.setGmtCreate(new Date(time));
@@ -69,13 +75,22 @@ public class LendImpl implements LendService {
     @Override
     public int updateByPrimaryKeySelective(BooksBorrow record){
         BooksBorrow booksBorrow = booksBorrowMapper.selectByPrimaryKey(record.getPkId());
+        if(booksBorrow==null)
+            return 0;
         if(record.getIsReturn()==1)
-            record.setActualReturn(new Date());
-        if(booksBorrowMapper.updateByPrimaryKeySelective(record)>0){
+            booksBorrow.setActualReturn(new Date());
+        if(booksBorrowMapper.updateByPrimaryKeySelective(booksBorrow)>0){
             Books books = new Books();
             books.setPkId(booksBorrow.getBooksId());
             books.setStatus(0);
-            return booksMapper.updateByPrimaryKeySelective(books);
+            if(booksMapper.updateByPrimaryKeySelective(books)>0){
+                if(booksBorrow.getFine().compareTo(new BigDecimal(0))>0){
+                    Users users = usersMapper.selectByPrimaryKey(booksBorrow.getUserId());
+                    users.setBalance(users.getBalance()+booksBorrow.getFine().intValue());
+                    users.setNum(users.getNum()-1);
+                    return usersMapper.updateByPrimaryKeySelective(users);
+                }
+            }
         }
         return 0;
     }
